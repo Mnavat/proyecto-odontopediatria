81843d0b0f434a5c07839443021044d0fd9b7844
Miguel Nava Tiburcio <mnavat96@gmail.com>
2023-07-07 12:33:24 -0600

    Componente scheduled agregado

--- webodontoped/src/app/components/schedule/schedule.component.ts
+++ webodontoped/src/app/components/schedule/schedule.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Cita } from 'src/app/models/cita';
+import { CitasService } from 'src/app/services/citas.service';
 
 @Component({
   selector: 'app-schedule',
   templateUrl: './schedule.component.html',
-  styleUrls: ['./schedule.component.css']
+  styleUrls: ['./schedule.component.css'],
+  providers: [CitasService]
 })
-export class ScheduleComponent {
+
+export class ScheduleComponent implements OnInit{
+  public title: string;
+  public cita: Cita;
+  
+
+  constructor(
+    private _citaService: CitasService
+  ){
+    this.title= "Agenda tu cita";
+    this.cita= new Cita("","","","","","","","");
+  }
+  
+  ngOnInit(){
+
+  }
+
+  onSubmit(form: any){
+    console.log(this.cita);
+  }
+
 
 }
