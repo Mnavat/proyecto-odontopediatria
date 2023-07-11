34ce20fa80ba0f951fada6cc6e08350c51ac839b
Miguel Nava Tiburcio <mnavat96@gmail.com>
2023-07-11 16:16:15 -0600

    actualización scheduled y el index de navegador

--- webodontoped/src/app/app.module.ts
+++ webodontoped/src/app/app.module.ts
@@ -9,6 +9,11 @@ import { AboutComponent } from './components/about/about.component';
 import { ScheduleComponent } from './components/schedule/schedule.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { ServicesofferComponent } from './components/servicesoffer/servicesoffer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatInputModule } from "@angular/material/input";
+import { MatNativeDateModule } from "@angular/material/core";
+
 
 @NgModule({
   declarations: [
@@ -22,7 +27,11 @@ import { ServicesofferComponent } from './components/servicesoffer/servicesoffer
     BrowserModule,
     routing,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatNativeDateModule
   ],
   providers: [
     appRoutingProviders
--- webodontoped/src/app/components/schedule/schedule.component.ts
+++ webodontoped/src/app/components/schedule/schedule.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Cita } from 'src/app/models/cita';
 import { CitasService } from 'src/app/services/citas.service';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatNativeDateModule} from '@angular/material/core';
 
 @Component({
   selector: 'app-schedule',
   templateUrl: './schedule.component.html',
   styleUrls: ['./schedule.component.css'],
-  providers: [CitasService]
+  providers: [CitasService],
 })
 
 export class ScheduleComponent implements OnInit{
   public title: string;
   public cita: Cita;
-  
 
   constructor(
     private _citaService: CitasService
