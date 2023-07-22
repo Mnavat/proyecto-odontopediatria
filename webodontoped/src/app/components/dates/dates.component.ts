894076083279cf6b02e33a18340127af6b7cd140
Andre Nava <jandrenavat@gmail.com>
2023-07-21 18:17:01 -0600

    Agregando por GET a los pacientes en el componente citas agendadas

--- webodontoped/src/app/components/dates/dates.component.ts
+++ webodontoped/src/app/components/dates/dates.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Cita } from 'src/app/models/cita';
+import { CitaService } from 'src/app/services/citas.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dates',
   templateUrl: './dates.component.html',
-  styleUrls: ['./dates.component.css']
+  styleUrls: ['./dates.component.css'],
+  providers: [CitaService],
 })
-export class DatesComponent {
+export class DatesComponent implements OnInit {
+  public title: string;
+  public cita: Cita;
+  listCitas: Cita[] = [];
+
+  constructor(
+    private _citaService: CitaService,
+    private router: Router
+  ){
+    this.title= "Citas agendadas";//original
+    this.cita= new Cita("","","","","","","",""); //original
+
+  }
+
+  ngOnInit(): void {
+    this.obtenerCitas()
+  }
+
+  obtenerCitas(){
+    this._citaService.getCitas().subscribe({
+      next: response => this.listCitas=response,
+      error: error => console.log(error)
+    });
+  }
 
 }
